4543c61d778c30ef786db20df00d44471b78b40c
Lasse <s195165@dtu.dk>
2022-06-09 12:49:27 +0200

    Player lock in handling

--- SPringboot/demo/src/main/java/com/example/demo/Game/Games.java
+++ SPringboot/demo/src/main/java/com/example/demo/Game/Games.java
@@ -4,9 +4,17 @@ public class Games {
     private int totalPlayers;
     private int playerCounter;
 
+    private String gameName;
     private Player[] gamePlayers;
 
-    Games(int totalPlayers, String username){
+    private int playerTurn;
+
+    private boolean allLocked;
+    private boolean gameLive;
+
+    Games(int totalPlayers, String username, String gameName){
+        gameName = this.gameName;
+        gameLive = false;
         totalPlayers = this.totalPlayers;
         gamePlayers = new Player[totalPlayers];
 
@@ -15,5 +23,31 @@ public class Games {
 
     private void addPlayer(String username){
         gamePlayers[playerCounter] = new Player(username);
+        playerCounter++;
+
+        if(totalPlayers == playerCounter){
+            startGame();
+        }
+    }
+
+    public void setPlayerLocked(){
+        boolean lockFlag = true;
+
+        for(int i = 0; i < totalPlayers; i++){
+            if(!gamePlayers[i].getLocked()){
+                lockFlag = false;
+            }
+        }
+
+        allLocked = lockFlag;
+    }
+
+    private void startGame(){
+        gameLive = true;
+        allLocked = false;
+    }
+
+    public int getPlayerNumber(){
+        return playerCounter;
     }
 }
--- SPringboot/demo/src/main/java/com/example/demo/Game/Player.java
+++ SPringboot/demo/src/main/java/com/example/demo/Game/Player.java
@@ -2,8 +2,14 @@ package com.example.demo.Game;
 
 public class Player {
     private String username;
+    private boolean locked;
 
     Player(String username){
         username = this.username;
+        locked = false;
+    }
+
+    public boolean getLocked(){
+        return locked;
     }
 }
